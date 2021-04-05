9ffb491d4733e6acaaf015b8bbbb6bd1c3a82a8d
Daniel <mail@oixbr.run>
2021-04-05 19:29:15 +0100

    .

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,10 +4,3 @@ sleep 1
 # Make internal Docker IP address available to processes.
 # Make internal Docker IP address available to processes.
 export INTERNAL_IP=`ip route get 1 | awk '{print $NF;exit}'`
-
-# Replace Startup Variables
-MODIFIED_STARTUP=`eval echo $(echo ${STARTUP} | sed -e 's/{{/${/g' -e 's/}}/}/g')`
-echo ":/mnt/server/$ ${MODIFIED_STARTUP}"
-
-# Run the Server
-eval ${MODIFIED_STARTUP}
